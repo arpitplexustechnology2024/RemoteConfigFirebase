109d63e2b7bacd5da76df49a50237f7f49f00c1d
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-21 17:37:50 -0700

    Commit

---
+++ Pods/Target Support Files/Pods-RemoteConfigFirebaseTests/Pods-RemoteConfigFirebaseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RemoteConfigFirebaseTests : NSObject
+@end
+@implementation PodsDummy_Pods_RemoteConfigFirebaseTests
+@end
--- RemoteConfigFirebase/ViewController.swift
+++ RemoteConfigFirebase/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import FirebaseRemoteConfig
 
 class ViewController: UIViewController {
 
+    let welcomeMessageConfigKey = "welcome_message"
+    let goodMorningMessageConfigKey = "goodmorning_message"
+    let welcomeMessageCapsConfigKey = "welcome_message_caps"
+    let loadingPhraseConfigKey = "loading_phrase"
+
+    var remoteConfig: RemoteConfig!
+    @IBOutlet var welcomeLabel: UILabel!
+    @IBOutlet var goodMorningLabel: UILabel!
+
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+      remoteConfig = RemoteConfig.remoteConfig()
+      let settings = RemoteConfigSettings()
+      settings.minimumFetchInterval = 0
+      remoteConfig.configSettings = settings
+
+      remoteConfig.setDefaults(fromPlist: "RemoteConfigDefaults")
+
+      fetchConfig()
     }
 
+    func fetchConfig() {
+      welcomeLabel.text = remoteConfig[loadingPhraseConfigKey].stringValue
+      goodMorningLabel.text = remoteConfig[loadingPhraseConfigKey].stringValue
 
-}
+      remoteConfig.fetch { (status, error) -> Void in
+        if status == .success {
+          print("Config fetched!")
+          self.remoteConfig.activate { changed, error in
+          }
+        } else {
+          print("Config not fetched")
+          print("Error: \(error?.localizedDescription ?? "No error available.")")
+        }
+        self.displayWelcome()
+      }
+    }
 
+    func displayWelcome() {
+      var welcomeMessage = remoteConfig[welcomeMessageConfigKey].stringValue
+      var goodMorningMessage = remoteConfig[goodMorningMessageConfigKey].stringValue
+
+      if remoteConfig[welcomeMessageCapsConfigKey].boolValue {
+        welcomeMessage = welcomeMessage?.uppercased()
+          goodMorningMessage = goodMorningMessage?.uppercased()
+      }
+      welcomeLabel.text = welcomeMessage
+        goodMorningLabel.text = goodMorningMessage
+    }
+
+  }
